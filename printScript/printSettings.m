ac4c74fee6570c7ad3d39052c7a0c65e4e10fc0a
Carlo Ferrara <carlo.ferrara.2000@gmail.com>
2024-07-09 23:21:51 +0200

    [MARGIN]: Now prints good, TODO
    Fix "Frequency" appearing instead of phase

--- printScript/printSettings.m
+++ printScript/printSettings.m
@@ -4,7 +4,9 @@ set(findall(hfig1,'-property','Interpreter'),'Interpreter','latex')
 set(findall(hfig1,'-property','TickLabelInterpreter'),'TickLabelInterpreter','latex')
 
 set(findall(hfig1,'-property','FontSize'),'FontSize', 20)
-set(findall(lgd1, '-property', 'FontSize'), 'FontSize', 16)
+if (~isempty(lgd1))
+	set(findall(lgd1, '-property', 'FontSize'), 'FontSize', 16)
+end
 
 set(findall(ttl1, '-property', 'FontSize'), 'FontSize', 24)
 
