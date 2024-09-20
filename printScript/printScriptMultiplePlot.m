807f03e6bf63f2fc04ac1b37aade0eb89b85bdf7
Carlo Ferrara <carlo.ferrara.2000@gmail.com>
2024-09-20 12:27:26 +0200

    [MULTI_PLOTS]: Added possibility to draw multiple hundreds of lines on same figure

---
+++ printScript/printScriptMultiplePlot.m
@@ -0,0 +1,14 @@
+function [hfig1, ttl1] = printScriptMultiplePlot(title1, xLabel1, yLabel1, tLR, tLL)
+
+	hfig1 = figure;
+	hold on, grid on;
+	ttl1 = title(title1);
+	xlabel(xLabel1);
+	ylabel(yLabel1);
+	if (~isempty(tLR) && ~isempty(tLL))
+		plot(tLR.X, tLR.Y, "lineWidth", 2);
+		plot(tLL.X, tLL.Y, "lineWidth", 2);
+	end
+
+end
+
