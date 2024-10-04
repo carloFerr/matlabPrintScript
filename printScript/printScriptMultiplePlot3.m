7db929f0a648890c2224b01c4e675b01e3618757
Carlo Ferrara <carlo.ferrara.2000@gmail.com>
2024-10-04 11:03:06 +0200

    [MULTI_PLOTS]: Added plotting of 3 multiple elements

---
+++ printScript/printScriptMultiplePlot3.m
@@ -0,0 +1,16 @@
+function [hfig1, ttl1] = printScriptMultiplePlot3(title1, xLabel1, yLabel1, tLR, tLL, tlSimRX, tlSimRY, tlSimLX, tlSimLY)
+
+	hfig1 = figure;
+	hold on, grid on;
+	ttl1 = title(title1);
+	xlabel(xLabel1);
+	ylabel(yLabel1);
+	if (~isempty(tLR) && ~isempty(tLL))
+		plot(tLR.X, tLR.Y, "lineWidth", 2);
+		plot(tLL.X, tLL.Y, "lineWidth", 2);
+		plot(tlSimRX, tlSimRY, "lineWidth", 2);
+		plot(tlSimLX, tlSimLY, "lineWidth", 2);
+	end
+
+end
+
